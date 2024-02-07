1e78217b0b2dbcb922224c9c91364cb596294978
cristiansandica <cristian.sandica17@gmail.com>
2024-02-07 17:13:10 +0200

    init the project

--- src/init.js
+++ src/init.js
@@ -1,6 +1,26 @@
 const TICK_RATE = 3000;
 
-
 function tick() {
-    console.log("tick", Date.now())
+  console.log("tick", Date.now());
 }
+
+async function init() {
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now(); //track next time we gonna call tick()
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+
+    requestAnimationFrame(nextAnimationFrame); //
+  }
+
+  requestAnimationFrame(nextAnimationFrame);
+}
+
+init();
